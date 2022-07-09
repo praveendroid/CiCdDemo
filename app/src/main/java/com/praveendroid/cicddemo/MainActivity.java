1d9233fbdf82c3421812c4c03407b3f2ea98c856
Praveen Singh Bora <praveendroid28@gmail.com>
2022-07-10 00:36:20 +0530

    Initial commit

---
+++ app/src/main/java/com/praveendroid/cicddemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.praveendroid.cicddemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
